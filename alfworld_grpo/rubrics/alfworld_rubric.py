2b175658147773aad8082e001ca36d7b8217a2bc
crislmfroes <cris.lima.froes@gmail.com>
2025-03-02 15:52:50 -0300

    Update rubric

--- alfworld_grpo/rubrics/alfworld_rubric.py
+++ alfworld_grpo/rubrics/alfworld_rubric.py
@@ -8,8 +8,8 @@ class AlfworldRubric(ToolRubric):
         self.reward_funcs = [
             self.success_reward_func,
             self.difflib_task_score_reward_func,
-            self.xml_reward_func,
-            self.format_reward_func,
+            self.parser.get_xml_reward_func,
+            self.parser.get_format_reward_func,
             self.tool_execution_reward_func
         ]
 
